fa68848666675108a7eb05413182dd8c0522560e
Dracon-151 <demianalexisalvarado@gmail.com>
2023-05-15 16:29:30 -0700

    Avance en diseno vista de proyectos 90% lista

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="px-4 py-2 mb-4 w-100 headerBar text-light d-flex">
+    <div class="px-4 py-2 w-100 headerBar text-light d-flex">
         <h3 class="mt-2 col">{{ user.name }} {{ user.lastname }}</h3>
 
         <div class="mt-2">
@@ -13,35 +13,43 @@
             <input type="hidden" name="_token" :value="csrf" />
         </form>
     </div>
-    <div class="row w-100 px-5">
-        <div class="col-3" v-for="(project, index) in filteredProjects" :key="project.id">
-            <div class="card p-3 h-100">
-                <a :href="route('draws.show', project.id)">
-                    <h5>{{ project.name }}</h5>
-                    <h6 class="text-muted"> Abierto el {{ formatDateHourTextShort(project.updated_at) }}</h6>
-                </a>
-                <button class="btn btn-danger mt-2" @click="destroy(project.id, index)">
-                    <i class="me-2 text-light fa-solid fa-trash"></i>
-                    Eliminar
-                </button>
+    <div class="background pt-5">
+        <div class="w-100 px-5">
+            <div class="bg-card card px-4 pb-5">
+                <h2 class="my-4">Mis proyectos</h2>
+                <div class="row">
+                    <div class="col-3 mb-4" v-for="(project, index) in filteredProjects" :key="project.id">
+                        <div class="project-card card p-3 h-100">
+                            <a :href="route('draws.show', project.id)">
+                                <h5>{{ project.name }}</h5>
+                                <h6 class="text-muted"> Abierto el {{ formatDateHourTextShort(project.updated_at) }}</h6>
+                            </a>
+                            <button class="btn btn-danger mt-2" @click="destroy(project.id, index)">
+                                <i class="me-2 text-light fa-solid fa-trash"></i>
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="project-card card p-3 h-100">
+                            <a :href="route('draws.store')">
+                                <h5>Nuevo proyecto</h5>
+                                <h6 class="text-muted"> Crea un proyecto en blanco</h6>
+                                <button class="btn btn-success mt-2 w-100">
+                                    <i class="me-2 text-light fa-solid fa-plus"></i>
+                                    Crear
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-3">
-            <div class="card p-3 h-100">
-                <a :href="route('draws.store')">
-                    <h5>Nuevo proyecto</h5>
-                    <h6 class="text-muted"> Crea un proyecto en blanco</h6>
-                    <button class="btn btn-success mt-2 w-100">
-                        <i class="me-2 text-light fa-solid fa-plus"></i>
-                        Crear
-                    </button>
-                </a>
+            <div class="mt-4">
+                <h6 class="text-light"> Mostrando {{ filteredProjects.length }} de {{ projects.length }} proyectos</h6>
             </div>
-        </div>
-        <div class="col-12 mt-4">
-            <h6 class="text-muted"> Mostrando {{ filteredProjects.length }} de {{ projects.length }} proyectos</h6>
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -67,7 +75,7 @@ export default {
                 var filter = true;
 
                 if(searchString.value.length> 0){
-                    filter = project.name.toUpperCase().includes(searchString.value.toUpperCase())
+                    filter = project.name?.toUpperCase().includes(searchString.value.toUpperCase())
                 }
 
                 return filter;
@@ -123,10 +131,22 @@ export default {
 a{
     text-decoration: none;
 }
-.card{
+.project-card{
     cursor: pointer;
 }
-.card:hover {
+.project-card:hover {
   box-shadow: 0 0 11px rgba(33,33,33,.2); 
+}
+.bg-card{
+    background-color: #EEEEEE;
+}
+.background{
+    background-image: url("/images/fondo.png");
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    height: 89.75vh;
+    width: 100vw;
 }
 </style>
--- resources/views/draws/show.blade.php
+++ resources/views/draws/show.blade.php
@@ -5,13 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js" integrity="sha512-3RlxD1bW34eFKPwj9gUXEWtdSMC59QqIqHnD8O/NoTwSJhgxRizdcFVQhUMFyTp5RwLTDL0Lbcqtl8b7bFAzog==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://kit.fontawesome.com/9b3bebdb5e.js" crossorigin="anonymous"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{$draw->name}}</title>
     
-<script src=""></script>
-
-    <title>Document</title>
 </head>
 
 <body>
+    {{$draw}}
 
     <input type="text" value="0" id="relx">
     <input type="text" value="0" id="rely">
@@ -25,6 +28,20 @@
     <button onclick="addColor('red')">rojo</button>
     <div id="sketch-holder">
     </div>
+
+    
+    <div id="app">
+        {{--<projects-show
+            :user='@json(Auth::user())'
+            :project='@json($draw)'
+        ></projects-show>--}}
+    </div>
+
+    <!--Rutas en vue-->
+    @routes
+
+    <!--VUE-->
+    @vite('resources/js/app.js')
 </body>
 
 
